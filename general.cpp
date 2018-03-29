4157a459073650530d2bd679ccb272a4d449b3bc
Erik Frojdh <erik.frojdh@gmail.com>
2018-03-29 20:48:03 +0200

    fix

--- general.cpp
+++ general.cpp
@@ -41,25 +41,26 @@ int main()
     //C style error code
     double result1 = 0;
     if (division1(23, 0, result1))
-        std::cout << "Error in division\n";
+        std::cout << "Division by zero in division1\n";
     else
         std::cout << "Result1: " << result1 << "\n";
 
     //Error code by reference
     int error_code = 0;
-    double result2 = division2(4, 5, error_code);
-    std::cout << "Result2: " << result2 << "\n";
+    double result2 = division2(4, 0, error_code);
+    if (error_code)
+        std::cout << "Division by zero in division2\n";
 
     //Exception
     double result3 = 0;
     try
     {
-        result3 = division3(5, 0);
+        double result3 = division3(5, 0);
     }
     catch (const std::runtime_error &e)
     {
         std::cout << "Caught exception in division3: \"" << e.what() << "\"\n";
     }
-    std::cout << "Result3: " << result3 << "\n";
+
     return 0;
 }
